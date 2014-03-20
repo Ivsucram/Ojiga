90740f59820c4e30111f2a160dfc6bcecb7c8df6
Marcus Vinicius <ivsucram@gmail.com>
2014-03-20 16:09:33 +0800

    Generate Scaffold User

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :nick, :email, :birthday, :picture, :isTermConditionsChecked, :created_at, :updated_at
---
+++ db/migrate/20140320080725_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :nick
+      t.string :email
+      t.date :birthday
+      t.string :picture
+      t.boolean :isTermConditionsChecked
+
+      t.timestamps
+    end
+  end
+end
