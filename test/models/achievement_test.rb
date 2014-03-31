64492fcf27e216e2a0248c12bd747e7a55c27b00
Marcus Vinicius <ivsucram@gmail.com>
2014-03-31 18:15:36 +0800

    Model Achievement - Unit tests validates fields user and name

--- test/models/achievement_test.rb
+++ test/models/achievement_test.rb
@@ -42,4 +42,30 @@ class AchievementTest < ActiveSupport::TestCase
   	end
   end
 
+  test 'validates field user' do
+  	# Validates presence
+  	achievement = Achievement.new(name: 'AchievementName')
+  	assert !achievement.save
+
+  	# Validates happy route
+  	assert_difference('Achievement.count') do
+  		Achievement.create(user: @user, name: 'AchievementName')
+  	end
+  end
+
+  test 'validates field name' do
+  	# Validates presence
+  	achievement = Achievement.new(user: @user)
+  	assert !achievement.save
+
+  	# Validates length
+	achievement = Achievement.new(user: @user, name: '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901')
+  	assert !achievement.save
+
+  	# Validates happy route
+  	assert_difference('Achievement.count') do
+  		Achievement.create(user: @user, name: '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890')
+  	end
+  end
+ 
 end
